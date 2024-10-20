e57f7618db60cc59abd83d51928c2b5d41caecec
samwel141 <samwelwilson141@gmail.com>
2024-10-20 09:59:52 +0300

    fix: fix callback url

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const TMDB_API_KEY = process.env.TMDB_API_KEY;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://movies-app-tau-teal.vercel.app/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let users = readUsersFromFile();
