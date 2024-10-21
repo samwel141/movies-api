30317468455ba42a8111ca32027b3bc8e16e58e0
samwel141 <samwelwilson141@gmail.com>
2024-10-21 06:37:40 +0300

    fix: fix google login

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String },
     email: { type: String, required: true, unique: true },
     googleId: { type: String },
     watch_list: [{ type: String }],
